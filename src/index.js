969e909799acac5837c8eb09da4a4a3edd2a38eb
Neha Jaiswal <nehajaiswal694@gmail.com>
2023-01-23 12:16:16 +0530

    addsrc

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const route = require("./route/route")
+const app = express()
+app.use(express.json())
+
+
+mongoose.connect("mongodb+srv://nehajaiswal:neha123@nehadb.pcorgpc.mongodb.net/group15Database", {
+    useNewUrlParser: true,
+})
+    .then(() => console.log("DB is Connected"))
+    .catch(error => console.log(error)
+
+app.use("/", route)
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000")
+})
