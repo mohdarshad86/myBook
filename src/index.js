96f2082267a2a2bba2fe993f8dd40025916612b0
GauravDattuPise <GauravDattuPise>
2023-01-30 13:13:28 +0530

    final

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ const express = require("express")
 const mongoose = require("mongoose")
 const route = require("./route/route")
 const app = express()
-var cors = require('cors')
+//var cors = require('cors')
 
 app.use(express.json())
 
-app.use(cors())
+//app.use(cors())
 mongoose.set('strictQuery', true)
 
 mongoose.connect("mongodb+srv://nehajaiswal:neha123@nehadb.pcorgpc.mongodb.net/group15Database", {
