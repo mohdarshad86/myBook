12774850a7a396966c4b68c0063d7f86674abf79
Mohd Arshad <mohdarshad8668@gmail.com>
2023-01-30 23:04:51 +0530

    added validations

--- src/validations/validation.js
+++ src/validations/validation.js
@@ -1,13 +1,10 @@
 const validate = function(a){
-if(a.match (/^([a-z A-Z]){2,50}$/)) return true
+if(a.match (/^(?=.{1,50}$)[a-z]+(?:['_.\s][a-z]+)*$/i)) return true
 }
 
 const validateTitle = function(a){
 if(a.match (/^([a-z A-Z\d]){2,50}$/)) return true
 }
-// const validateObjectId = function(a){
-//     if(a.match(/^[a-f\d]{24}$/i)) return true;
-// }
 
 const validateISBN= function(a){
     if(a.match(/^(?:ISBN(?:-1[03])?:? )?(?=[0-9X]{10}$|(?=(?:[0-9]+[- ]){3})[- 0-9X]{13}$|97[89][0-9]{10}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)(?:97[89][- ]?)?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9X]$/i)) return true
@@ -25,5 +22,13 @@ const validatePhone = function(a){
     if(a.match(/^[6-9][0-9]{9}$/)) return true
 }
 
+const validatePincode = (pincode) => {
+    if(a.match(/^[1-9][0-9]{5}$/)) return true
+}
 
-module.exports={validate,validateTitle,validateISBN,validateEmail,validatePassword,validatePhone}
+const validatePlace = (value) => {
+    if(a.match(/^[^\W\d_]+\.?(?:[-\s'’][^\W\d_]+\.?)*$/)) return true
+}
+
+
+module.exports={validate,validateTitle,validateISBN,validateEmail,validatePassword,validatePhone,validatePincode,validatePlace}
